638f515f52b0e7b453c23c4ec1c02e2b629eab89
matigames765 <134336140+matigames765@users.noreply.github.com>
2023-08-24 19:40:17 -0300

    Add files via upload

---
+++ Trabajo practico nro 2 programacion 1/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Ingrese la primer nota: "))
+nota2 = float(input("Ingrese la segunda nota: "))
+nota3 = float(input("Ingrese la tercer nota: "))
+nota4 = float(input("Ingrese la cuarta nota: "))
+promedio = (nota1 + nota2 + nota3 + nota4) / 4
+if (promedio >= 6):
+    print("Esta aprobado")
+else:
+    print("Esta desaprobado")
